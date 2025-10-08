e1d7be678f221f364034ac3717ac48fd14205956
Gareth <143119785+gargum@users.noreply.github.com>
2025-10-08 15:46:11 -0400

    Updated mcuconf.h
    Added the code to explicitly specify the use of the SIO driver and removed a redundant #undef

--- RP2040_Firmware/mcuconf.h
+++ RP2040_Firmware/mcuconf.h
@@ -3,5 +3,5 @@
 #include_next <mcuconf.h>
 
 // for the serial connection between the two keyboard halves
-    #undef RP_SERIAL_USE_USART0
     #define RP_SERIAL_USE_USART0 TRUE
+    #define RP_SIO_USE_UART0 TRUE
